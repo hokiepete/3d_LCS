24210a13580293465397b88e5cb99879febfefa6
Peter <pnolan1986@gmail.com>
2018-07-10 20:06:43 -0400

    fixes

--- 3dftle.py
+++ 3dftle.py
@@ -16,17 +16,37 @@ from skimage.draw import ellipsoid
 
 #f = hp.File('ftle.mat','r')
 array = sio.loadmat('ftle.mat')
-ftle = array['f']
+ftle = array['f'][:,::4,:]
+eig1 = array['f'][:,1::4,:]
+eig2 = array['f'][:,2::4,:]
+eig3 = array['f'][:,3::4,:]
 del array
 
+dim = ftle.shape
+dfdy, dfdx, dfdz = np.gradient(ftle)
+dydy, dydx, dydz = np.gradient(dfdy)
+dxdy, dxdx, dxdz = np.gradient(dfdx)
+dzdy, dzdx, dzdz = np.gradient(dfdz)
 
+dirdiv = np.empty(dim)
+concav = np.empty(dim)
+for i in range(dim[0]):
+    for j in range(dim[1]):
+        for k in range(dim[2]):
+            dirdiv[i,j,k] = np.dot([dfdx[i,j,k],dfdy[i,j,k],dfdz[i,j,k]],[eig1[i,j,k],eig2[i,j,k],eig3[i,j,k]])
+            concav[i,j,k] = np.dot(np.dot([[dxdx[i,j,k],dxdy[i,j,k],dxdz[i,j,k]],[dydx[i,j,k],dydy[i,j,k],dydz[i,j,k]],[dzdx[i,j,k],dzdy[i,j,k],dzdz[i,j,k]]],[eig1[i,j,k],eig2[i,j,k],eig3[i,j,k]]),[eig1[i,j,k],eig2[i,j,k],eig3[i,j,k]])
+
+dirdiv = np.ma.masked_where(concav>=0,dirdiv)
+'''
 # Generate a level set about zero of two identical ellipsoids in 3D
 ellip_base = ellipsoid(6, 10, 16, levelset=True)
 ellip_double = np.concatenate((ellip_base[:-1, ...],
-                               ellip_base[2:, ...]), axis=0)
-
+                              ellip_base[2:, ...]), axis=0)
+'''
 # Use marching cubes to obtain the surface mesh of these ellipsoids
-verts, faces, normals, values = measure.marching_cubes_lewiner(ftle, 0)
+print "Plot Time"
+verts, faces, normals, values = measure.marching_cubes_lewiner(dirdiv, 0)
+#verts, faces, normals, values = measure.marching_cubes_lewiner(ellip_double, 0)
 
 # Display resulting triangular mesh using Matplotlib. This can also be done
 # with mayavi (see skimage.measure.marching_cubes_lewiner docstring).
@@ -42,9 +62,10 @@ ax.set_xlabel("x-axis: a = 6 per ellipsoid")
 ax.set_ylabel("y-axis: b = 10")
 ax.set_zlabel("z-axis: c = 16")
 
-ax.set_xlim(0, 24)  # a = 6 (times two for 2nd ellipsoid)
-ax.set_ylim(0, 20)  # b = 10
-ax.set_zlim(0, 32)  # c = 16
+ax.set_xlim(0, 258)  # a = 6 (times two for 2nd ellipsoid)
+ax.set_ylim(0, 256)  # b = 10
+ax.set_zlim(0, 40)  # c = 16
 
 plt.tight_layout()
-plt.show()
+plt.savefig('FTLE.png', transparent=True, bbox_inches='tight')
+#plt.show()
